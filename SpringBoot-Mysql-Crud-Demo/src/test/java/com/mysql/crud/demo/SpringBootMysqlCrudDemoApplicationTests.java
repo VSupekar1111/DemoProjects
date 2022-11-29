611e04ff2e190351fb9f4b96c9071efb085ef3c5
unknown <cogjava4465@iiht.tech>
2022-11-29 12:10:55 +0530

    Demo Project added-Mysql CRUD

---
+++ SpringBoot-Mysql-Crud-Demo/src/test/java/com/mysql/crud/demo/SpringBootMysqlCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysql.crud.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMysqlCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
